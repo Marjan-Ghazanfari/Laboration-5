67be2e7404a874e7871abf0047221c293925e37e
Marjan Ghazanfari <ghazanfari.marjan@gmail.com>
2021-03-17 12:30:58 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let num1 = 40;
+let num2 = 30;
+
+console.log(`${num1} + ${num2} = ${num1+num2}`);
+console.log(`${num1} - ${num2} = ${num1-num2}`);
+console.log(`${num1} * ${num2} = ${num1*num2}`);
+
+const person = {
+    firstName: 'Marjan',
+    lastName: 'Ghazanfari',
+    age: 35
+};
+
+console.log(`Hej, mitt namn är ${person.firstName} ${person.lastName} och jag är ${person.age} år gammal.`);
